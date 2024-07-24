b4ec7bc478c97ed9a919c70bcdc412b45ba5a8f9
azuki <yuhi55.k@gmail.com>
2024-07-24 17:02:13 +0900

    Dictonary追加。ステータスのDictonary作成関数追加

--- TowerDefense/Assets/Test/Data/Magic/Status_Magic.cs
+++ TowerDefense/Assets/Test/Data/Magic/Status_Magic.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(menuName = "Data/StatusData_Magic")]
 
 public class Status_Magic : ScriptableObject
@@ -9,9 +10,38 @@ public class Status_Magic : ScriptableObject
     [SerializeField, Header("説明文")]
     string explanatoryNote;
 
+    [SerializeField,Header("必要なステータス")]
+    public List<MagicKeyValuePair> keyValuePairs;
+
+    private Dictionary<string, float> dictionary;
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Initialize()
+    {
+        dictionary = new Dictionary<string, float>();
+        foreach (MagicKeyValuePair kvp in keyValuePairs)
+        {
+            dictionary.Add(kvp.m_keyName, kvp.m_keyValue);
+        }
+    }
+
+
+
     public string GetExplanatoryNote()
     {
         return explanatoryNote;
+    }
+
+    /// <summary>
+    /// ステータスを管理するリストを取得
+    /// </summary>
+    /// <returns></returns>
+    public Dictionary<string, float> GetKeyValuePairs()
+    {
+        return dictionary;
     }
 
 }
